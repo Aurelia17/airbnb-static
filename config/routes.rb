e8f215c040f88b72a0c997eae929386b7e4b0e80
aurelia ritter <aureliaritter@gmail.com>
2024-10-10 19:30:15 +0200

    routes commentaires

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "flats#index"
-  get 'flats', to: "flats#index"
+  get 'flats', to: "flats#index" # => http://localhost:3000/flats
+  # verb 'path', to: "controller#action"
+  # path is the part after http://localhost:3000/ in url
 end
